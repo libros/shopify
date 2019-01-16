6ca88ef3e8d95c5e81e1a6ddcb7ed13c22422fc5
joel.meng <joel.mong.au@gmail.com>
2019-01-16 12:40:08 +1000

    Quick setup

--- ShopifyRepo/AppDelegate.swift
+++ ShopifyRepo/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let shopifyRepoTableView = UITableViewController(nibName: "\(ShopifyRepoTableViewController.self)", bundle: nil)
+        window?.rootViewController = UINavigationController(rootViewController: shopifyRepoTableView)
+        window?.makeKeyAndVisible()
         return true
     }
 
